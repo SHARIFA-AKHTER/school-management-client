eec1924d4905edf187ba2eec08702fa0ea19e98f
unknown <sr0589071@gmail.com>
2026-06-15 18:21:02 +0600

    created useSearchParams for usePayments

--- app/(dashboard)/payments/usePayments.ts
+++ app/(dashboard)/payments/usePayments.ts
@@ -1,97 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-// /* eslint-disable @typescript-eslint/no-unused-vars */
-// /* eslint-disable react-hooks/set-state-in-effect */
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// import { authService } from "@/app/services/api.service";
-// import { useState, useEffect } from "react";
-
-// import { toast } from "react-hot-toast";
-
-// export const usePayments = () => {
-//   const [payments, setPayments] = useState<any[]>([]);
-//   const [loading, setLoading] = useState<boolean>(true);
-//   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-//   const [submitting, setSubmitting] = useState<boolean>(false);
-
-//   const [formData, setFormData] = useState({
-//     amount: "",
-//     purpose: "Exam Fee",
-//     studentId: "",
-//   });
-
-//   const fetchPayments = async () => {
-//     try {
-//       setLoading(true);
-
-//       const response: any = await authService.getPayments();
-
-//       if (response?.data?.success && Array.isArray(response?.data?.data)) {
-//         setPayments(response.data.data);
-//       } else if (Array.isArray(response?.data)) {
-//         setPayments(response.data);
-//       }
-//     } catch (error) {
-//       toast.error("Failed to load historical database ledgers");
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   const handleCreatePayment = async (e: React.FormEvent) => {
-//     e.preventDefault();
-//     const { amount, purpose, studentId } = formData;
-
-//     if (!amount || !studentId || !purpose) {
-//       return toast.error("All input parameters are mandatory!");
-//     }
-
-//     try {
-//       setSubmitting(true);
-
-//       const response: any = await authService.initiatePayment({
-//         amount: Number(amount),
-//         purpose,
-//         studentId,
-//       });
-
-//       const redirectUrl = response?.data?.url || response?.data?.data?.gatewayUrl || response?.data?.data;
-
-//       if (redirectUrl) {
-//         toast.success("Redirecting to SSLCommerz Secure Gateway...");
-
-//         window.location.href = redirectUrl;
-//       } else {
-//         toast.error("Failed to fetch gateway deployment session url");
-//       }
-//     } catch (error: any) {
-//       toast.error(
-//         error.response?.data?.message || "Internal core server transaction handshake error"
-//       );
-//     } finally {
-//       setSubmitting(false);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchPayments();
-//   }, []);
-
-//   return {
-//     payments,
-//     loading,
-//     isModalOpen,
-//     setIsModalOpen,
-//     formData,
-//     setFormData,
-//     submitting,
-//     handleCreatePayment,
-//     refetchPayments: fetchPayments
-//   };
-// };
 
 /* eslint-disable react-hooks/set-state-in-effect */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { authService } from "@/app/services/api.service";
+import { useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
 
@@ -101,11 +13,29 @@ export const usePayments = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [submitting, setSubmitting] = useState<boolean>(false);
 
+  const searchParams = useSearchParams()
   const [formData, setFormData] = useState({
     amount: "",
     purpose: "Exam Fee",
     studentId: "",
   });
+
+  useEffect(()=>{
+    const status = searchParams.get("status");
+    const trx = searchParams.get("trx");
+    if (status ==="success") {
+      toast.success(`Payment completed successfully!" TrxID: ${trx}`,{
+            duration: 5000
+      });
+      window.history.replaceState({}, document.title, window.location.pathname)
+    }else if (status === "fail"){
+      toast.error("Payment failed! please try again." );
+      window.history.replaceState({}, document.title, window.location.pathname)
+    }else if (status === "cancel") {
+      toast.error("Payment cancelled by user." );
+      window.history.replaceState({}, document.title, window.location.pathname);
+    }
+  }, [searchParams])
 
   const fetchPayments = async () => {
     try {
